3561f02cd34349cb39ba6ab5fdd258ffa309e39f
Maxim <maxsavikin@gmail.com>
2025-07-31 12:08:26 +0300

    Добавлена обработка ошибок при регистрации и входе

--- components/auth/LoginScreen.js
+++ components/auth/LoginScreen.js
@@ -4,92 +4,91 @@ import {useNavigation} from "@react-navigation/native";
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function LoginScreen() {
-  const navigation = useNavigation();
+    const navigation = useNavigation();
 
-  const [userName, setUserName] = useState('');
-  const [userPassword, setUserPassword] = useState('');
+    const [userName, setUserName] = useState('');
+    const [userPassword, setUserPassword] = useState('');
 
-  const data = {
-    username: userName,
-    password: userPassword
-  };
+    const data = {
+        username: userName,
+        password: userPassword
+    };
 
-  const login = () => {
-    fetch('http://192.168.0.143:8000/api/login/', {
-      method: 'POST',
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify(data)
-    })
-      .then(res => {
-        if (!res.ok) {
-          throw new Error(`Ошибка входа: ${res.status}`);
-        }
-        return res.json();
-      })
-      .then(async data => {
-        if (data.token) {
-          await AsyncStorage.setItem('authToken', data.token);
-          navigation.navigate('Profile');
-        } else {
-          Alert.alert('Ошибка', 'Токен не получен');
-        }
-      })
-      .catch(error => {
-        console.error('Ошибка входа: ', error);
-        Alert.alert('Ошибка', 'Не удалось войти');
-      });
-  };
+    const login = () => {
+        fetch('http://192.168.0.143:8000/api/login/', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(data)
+        })
+            .then(res => {
+                if (!res.ok) {
+                    Alert.alert('Ошибка входа', "Неверное имя или пароль")
+                    throw new Error(`Ошибка входа: ${res.status}`)
 
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Вход</Text>
+                }
+                return res.json();
+            })
+            .then(async data => {
+                if (data.token) {
+                    await AsyncStorage.setItem('authToken', data.token);
+                    navigation.navigate('Profile');
+                } else {
+                    Alert.alert('Ошибка', 'Токен не получен');
+                }
+            })
 
-      <TextInput
-        style={styles.input}
-        placeholder="Введите ваше имя"
-        onChangeText={(text) => setUserName(text)}
-        value={userName}
-      />
+    };
 
-      <TextInput
-        style={styles.input}
-        placeholder="Введите ваш пароль"
-        secureTextEntry
-        onChangeText={(text) => setUserPassword(text)}
-        value={userPassword}
-      />
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>Вход</Text>
 
-      <View style={styles.buttonContainer}>
-        <Button title="Войти!" onPress={login} />
-      </View>
-    </View>
-  );
+            <TextInput
+                style={styles.input}
+                placeholder="Введите ваше имя"
+                onChangeText={(text) => setUserName(text)}
+                value={userName}
+            />
+
+            <TextInput
+                style={styles.input}
+                placeholder="Введите ваш пароль"
+                secureTextEntry
+                onChangeText={(text) => setUserPassword(text)}
+                value={userPassword}
+            />
+
+            <View style={styles.buttonContainer}>
+                <Button title="Войти!" onPress={login}/>
+            </View>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    paddingHorizontal: 30,
-    paddingVertical: 50,
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 26,
-    fontWeight: 'bold',
-    marginBottom: 30,
-    textAlign: 'center',
-    color: '#333',
-  },
-  input: {
-    borderWidth: 1,
-    borderColor: '#bbb',
-    borderRadius: 8,
-    padding: 10,
-    marginBottom: 20,
-    fontSize: 16,
-  },
-  buttonContainer: {
-    marginTop: 10,
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        paddingHorizontal: 30,
+        paddingVertical: 50,
+        justifyContent: 'center',
+    },
+    title: {
+        fontSize: 26,
+        fontWeight: 'bold',
+        marginBottom: 30,
+        textAlign: 'center',
+        color: '#333',
+    },
+    input: {
+        borderWidth: 1,
+        borderColor: '#bbb',
+        borderRadius: 8,
+        padding: 10,
+        marginBottom: 20,
+        fontSize: 16,
+    },
+    buttonContainer: {
+        marginTop: 10,
+    },
 });
--- components/auth/RegistrationScreen.js
+++ components/auth/RegistrationScreen.js
@@ -1,87 +1,96 @@
 import React, {useState} from 'react';
-import {View, TextInput, Button, Text, StyleSheet} from 'react-native';
+import {View, TextInput, Button, Text, StyleSheet, Alert} from 'react-native';
 import {useNavigation} from "@react-navigation/native";
 
 export default function RegistrationScreen() {
-  const navigation = useNavigation();
+    const navigation = useNavigation();
 
-  const [userName, setUserName] = useState('');
-  const [userPassword, setUserPassword] = useState('');
+    const [userName, setUserName] = useState('');
+    const [userPassword, setUserPassword] = useState('');
 
-  const data = {
-    username: userName,
-    password: userPassword
-  };
+    const data = {
+        username: userName,
+        password: userPassword
+    };
 
-  const register = () => {
-    fetch('http://192.168.0.143:8000/api/register/', {
-      method: 'POST',
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify(data)
-    })
-      .then(res => {
-        if (!res.ok) {
-          throw new Error(`Ошибка регистрации: ${res.status}`);
-        }
-        console.log('Регистрация прошла успешно');
-        return res.json();
-      })
-      .then(() => navigation.navigate('Profile'))
-      .catch(error => {
-        console.error(`Ошибка: ${error}`);
-      });
-  };
+    const register = () => {
 
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Регистрация</Text>
 
-      <TextInput
-        style={styles.input}
-        placeholder="Введите ваше имя"
-        onChangeText={setUserName}
-        value={userName}
-      />
+        fetch('http://192.168.0.143:8000/api/register/', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(data)
+        })
+            .then(res => {
+                if (!res.ok) {
+                    Alert.alert('Регистрация не удалась', "Пользователь с таким именем уже существует")
 
-      <TextInput
-        style={styles.input}
-        placeholder="Введите пароль"
-        secureTextEntry
-        onChangeText={setUserPassword}
-        value={userPassword}
-      />
+                    throw new Error('Ошибка при регистрации')
 
-      <View style={styles.buttonContainer}>
-        <Button title="Зарегистрировать!" onPress={register} />
-      </View>
-    </View>
-  );
+                }
+                console.log('Регистрация прошла успешно');
+                return res.json();
+            })
+            .then(() => navigation.navigate('Profile'))
+            .catch(error => {
+                if (error.message !== 'Ошибка при регистрации') {
+                    console.error('Ошибка: ', error)
+                    Alert.alert('Ошибка', "Что-то пошло не так")
+                }
+
+            })
+    }
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>Регистрация</Text>
+
+            <TextInput
+                style={styles.input}
+                placeholder="Введите ваше имя"
+                onChangeText={setUserName}
+                value={userName}
+            />
+
+            <TextInput
+                style={styles.input}
+                placeholder="Введите пароль"
+                secureTextEntry
+                onChangeText={setUserPassword}
+                value={userPassword}
+            />
+
+            <View style={styles.buttonContainer}>
+                <Button title="Зарегистрировать!" onPress={register}/>
+            </View>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    paddingHorizontal: 30,
-    paddingVertical: 50,
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 26,
-    fontWeight: 'bold',
-    marginBottom: 30,
-    textAlign: 'center',
-    color: '#333',
-  },
-  input: {
-    borderWidth: 1,
-    borderColor: '#bbb',
-    borderRadius: 8,
-    padding: 10,
-    marginBottom: 20,
-    fontSize: 16,
-  },
-  buttonContainer: {
-    marginTop: 10,
-  },
-});
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        paddingHorizontal: 30,
+        paddingVertical: 50,
+        justifyContent: 'center',
+    },
+    title: {
+        fontSize: 26,
+        fontWeight: 'bold',
+        marginBottom: 30,
+        textAlign: 'center',
+        color: '#333',
+    },
+    input: {
+        borderWidth: 1,
+        borderColor: '#bbb',
+        borderRadius: 8,
+        padding: 10,
+        marginBottom: 20,
+        fontSize: 16,
+    },
+    buttonContainer: {
+        marginTop: 10,
+    },
+})
